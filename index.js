b383cefffc91aafd64d1bb1d3daef359d8f6e416
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-06 13:55:50 +0700

    Implement regex

---
+++ index.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form')
+const output = document.getElementById('output')
+
+const checkInput = () => {
+  event.preventDefault()
+
+  const inputText = document.getElementById('input-text')
+
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  const email = inputText.value
+  const result = pattern.test(email)
+
+  if (result === true) {
+    output.setAttribute('class', 'valid')
+    output.innerHTML = 'Input is valid :)'
+  } else {
+    output.setAttribute('class', 'invalid')
+    output.innerHTML = 'Input is NOT valid :('
+  }
+}
